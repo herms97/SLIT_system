ef996c3310f2c45dcdbc3cdb86e76c2edb0a92da
Hakons1 <35727157+Hakons1@users.noreply.github.com>
2018-11-06 17:06:34 +0100

    Add files via upload

---
+++ SLITDATABASE.sql
@@ -0,0 +1,135 @@
+CREATE DATABASE IF NOT EXISTS SLIT;
+
+USE SLIT;
+
+create table if not exists user (
+user_id varchar (7) not null,
+user_name varchar (30) not null,
+password varchar (30) not null,
+email varchar (200) not null,
+primary key (user_id)
+);
+
+create table if not exists teacher (
+teacher_id varchar (7) not null,
+teacher_name varchar (16) not null,
+assistant_teacher boolean,
+user_id varchar (7),
+primary key (teacher_id),
+foreign key (user_id) references user(user_id)
+);
+
+create table if not exists student (
+student_id varchar (7) not null,
+student_name varchar (16),
+user_id varchar (7),
+primary key (student_id),
+foreign key (user_id) references user(user_id)
+);
+
+ create table if not exists course (
+ course_id varchar (7) not null,
+ course_name varchar (12),
+ primary key (course_id)
+ );
+ 
+ create table if not exists notifications (
+ notification_date DATETIME DEFAULT NOW(),
+ notification_info varchar (300)
+ );
+
+CREATE TABLE IF NOT EXISTS assignment (
+assignment_title VARCHAR(50),
+assignment_desc VARCHAR(500),
+assignment_due DATETIME
+);
+
+CREATE TABLE IF NOT EXISTS calendar (
+calendar_id VARCHAR(7) NOT NULL,
+calendar_title VARCHAR(50),
+calendar_desc VARCHAR(200)
+);
+
+CREATE TABLE IF NOT EXISTS delivery (
+delivery_title VARCHAR(50),
+delivery_demands VARCHAR (500),
+delivery_desc VARCHAR(500),
+delivery_due DATETIME
+);
+
+CREATE TABLE IF NOT EXISTS pointsystem (
+delivery_maxpoints int,
+delivery_leastpoints int,
+delivery_points int
+);
+
+CREATE TABLE IF NOT EXISTS feedback (
+feedback_date DATETIME DEFAULT NOW(),
+feedback_info VARCHAR(500)
+);
+
+
+
+INSERT INTO user (user_id, user_name, password, email)
+VALUES ('u001', 'teacher', 'password', 'teacher@gmail.com');
+
+INSERT INTO user (user_id, user_name, password, email)
+VALUES ('u002', 'student', 'passord', 'student@gmail.com');
+
+
+
+INSERT INTO teacher (teacher_id, teacher_name, assistant_teacher, user_id)
+VALUES ('t001', 'Hallgeir Nilsen', false, 'u001');
+
+
+
+INSERT INTO student (student_id, student_name, user_id)
+VALUES ('s001', 'Håkon Sveen', 'u002');
+
+
+
+INSERT INTO course (course_id, course_name)
+VALUES ('c001', 'IS-200');
+
+
+
+INSERT INTO notification (notification_info)
+VALUES ('Timen i morgen er kansellert pga. dårlig vær');
+
+
+
+INSERT INTO assignment (assignment_title, assignment_desc, assignment_due)
+VALUES ('Mandatory 1', 'Lag en  database', '2018-10-18 23:00:00');
+
+
+INSERT INTO calendar (calendar_id, calendar_title, calendar_desc)
+VALUES ('ca001', 'IT og informasjonssystemer bachelor høst 2018', 'beskrivelse av kalenderen');
+
+
+
+INSERT INTO delivery (delivery_title, delivery_demands, delivery_desc, delivery_due)
+VALUES ('Mandatory 4', 'create some queries in MySQL', 'extend the SLIT database', '2018-11-23 23:59:59');
+
+
+
+INSERT INTO pointsystem (delivery_maxpoints, delivery_leastpoints, delivery_points)
+VALUES (100, 0, 80);
+
+
+
+INSERT INTO feedback (feedback_date, feedback_info)
+VALUES ('2018-11-06 16:44:13', 'Bra jobba, men ser at dere kunne skrevet litt mer på oppgave 4A');
+
+
+
+
+drop table feedback;
+drop table pointsystem;
+drop table delivery;
+drop table calendar;
+drop table assignment;
+drop table notifications;
+drop table course;
+drop table student;
+drop table teacher;
+drop table user;
